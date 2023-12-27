eef0e02f919289f485ab95755fc966b2d52a5225
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-27 22:10:49 +0600

    populate database

--- backend/utils/createSongs.js
+++ backend/utils/createSongs.js
@@ -5,13 +5,33 @@ import Song from '../models/songModel.js';
 // Predefined list of tracks
 const predefinedTracks = [
     {
-      title: 'Track 1',
+      title: 'Tum hi ho',
       artist: 'Arijit Sing ',
       url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
     },
     {
-      title: 'Track 2',
-      artist: 'Artist 2',
+      title: 'Valobasha ',
+      artist: 'Nancy',
+      url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    },
+    {
+      title: 'Prem',
+      artist: 'James',
+      url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    },
+    {
+      title: 'Ek Mutho Rod',
+      artist: 'Balam',
+      url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    },
+    {
+      title: 'Ek Mutho Rod',
+      artist: 'Shera Ghoshal',
+      url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
+    },
+    {
+      title: 'Dil ibadat',
+      artist: 'KK',
       url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3',
     },
   ];
